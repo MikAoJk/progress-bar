f7a253cc809852644916e9cd39a7e7fe8ea55b17
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-09-06 07:55:59 +0200

    Update ProgressBar.tsx

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -9,7 +9,7 @@ const ProgressBar = ({ progress, progressText = "" }: ProgressBarProps) => {
   // Make sure our value stays between 0 and 100.
   const _progress = Math.min(Math.max(0, progress), 100);
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center w-1/4">
       <div className="w-full border-2 border-indigo-700 h-6 rounded-md">
         <div
           className="bg-indigo-500 h-full transition-all duration-250"
